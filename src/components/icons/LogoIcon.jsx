249fe75ccf4ffed7751a080a52a31a8267d74878
Leon <6860571@stud.nau.edu.ua>
2023-12-04 01:39:10 +0200

    Created animation

--- src/components/icons/LogoIcon.jsx
+++ src/components/icons/LogoIcon.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import logo from '../../assets/logo.svg'
+import classes from './LogoIcon.module.scss'
 
 const LogoIcon = ({ height = 26 }) => {
-  return <img src={logo} alt="logo" height={height} />
+  return <img src={logo} alt="logo" height={height} className={classes.image} />
 }
 
 export default LogoIcon
